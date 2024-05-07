9cd0ac28a5d61c4c95302f9749e7bb31a85a85d2
nollasalt <nollasalt@outlook.com>
2024-05-07 21:59:01 +0800

    version 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,12 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#include "solve.h"
+
 using namespace std;
 
-typedef pair<int,int> node;
 
-void solve(int source,vector<vector<node>> &graph);
-const int pMax=100,pMin=0;
-int main(){
-    vector<vector<node>> graph;
-    int source = 0;
-    solve(source,graph);
-    return 0;
-}
-
-void dfs(int fa,int u,int p,vector<vector<node>> &graph)
-{
-
-}
-void solve(int source,vector<vector<node>> &graph)
-{
-
+signed main() {
+    DAG g;
+    g.topsort();
+    g.branch_bound();
+    g.output_bst();
 }
