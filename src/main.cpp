daddad34222d7b03d4c922e54f61060326f5e65f
nollasalt <nollasalt@outlook.com>
2024-05-08 15:42:04 +0800

    test

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,23 @@ using namespace std;
 
 
 signed main() {
-    
-    DAG g;
-    g.topsort();
-//    g.backtracking(1,0);
-//    g.output_bst();
-//    cout<<"\n########"<<endl;
-    typedef std::chrono::high_resolution_clock Clock;
-    auto t1 = Clock::now();//计时开始
-    g.branch_bound();
-    g.output_bst();
-    auto t2 = Clock::now();//计时结束
-    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() << "ns" << endl;
+    //string pathOut="out/branchBound.ans";
+    string pathOut="out/backTrace.ans";
+    freopen(pathOut.c_str(),"w",stdout);
+    for (int i = 1; i <= 100; ++i) {
+        string pathIn="in/test";
+        pathIn+=to_string(i);
+        pathIn+=".in";
+        freopen(pathIn.c_str(),"r",stdin);
+        DAG g;
+        g.topsort();
+        typedef std::chrono::high_resolution_clock Clock;
+        auto t1 = Clock::now();//计时开始
+        //g.backTrace(2, 0);
+        g.branchBound();
+        //g.output_bst();
+        cout<<g.maxAns<<endl;
+        auto t2 = Clock::now();//计时结束
+        //std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() << "ns" << endl;
+    }
 }
--- src/solve.h
+++ src/solve.h
@@ -5,20 +5,20 @@ struct btnode {//用于分支定界的子集树
     btnode* parent;//指向父节点
     int press;//
     int level;//子集树层级
-    bool bst_here;//是否放放大器
-    int bstnum;//优先级 从根到节点x的路径上的bst个数
+    bool bstHere;//是否放放大器
+    int bstCnt;//优先级 从根到节点x的路径上的bst个数
 
     btnode() {
         parent = nullptr;
-        press = 0; level = 0; bst_here = false;
-        bstnum = 0;
+        press = 0; level = 0; bstHere = false;
+        bstCnt = 0;
     }
 
     btnode(int pres, int lev, btnode* par = nullptr, int bstn = 0) {
         parent = par;
         press = pres; level = lev;
-        bstnum = bstn;
-        bst_here = false;
+        bstCnt = bstn;
+        bstHere = false;
     }
 
 };
@@ -35,13 +35,13 @@ class Node {
 public:
     vector<Edge> edges;//记录所有出边
     int press;
-    bool booster;
-    int max_to_cost;
+    bool bstHere;
+    int maxToCost;
     Node() {
         press = 0;
-        booster = false;
+        bstHere = false;
         edges.clear();
-        max_to_cost = 0;
+        maxToCost = 0;
     }
     ~Node() {}
 };
@@ -49,7 +49,7 @@ public:
 struct cmp
 {
     bool operator () (const btnode* p1, const btnode* p2) {
-        return p1->bstnum > p2->bstnum;
+        return p1->bstCnt > p2->bstCnt;
     }
 };
 
@@ -58,9 +58,9 @@ private:
 
     int Pmin, Pmax;//
     vector<Node> nodes;// 邻接表
-    vector<int> in_deg;
-    vector<int> dig;//拓扑序列
-    vector<bool> place_bst;//是否要放在这
+    vector<int> inDegree;
+    vector<int> top;//拓扑序列
+    vector<bool> bstHere;//是否要放在这
     priority_queue<btnode*, vector<btnode*>, cmp> pq;//最小堆
 
 
@@ -69,129 +69,105 @@ public:
     DAG() {
         cin >> n >> m >> Pmax;
         Pmin = 0;
-        best_ans = INT_MAX;
+        maxAns = INT_MAX;
         nodes.clear();
-        in_deg.resize(n + 1, 0);
-        dig.resize(n + 1, 0);
-        place_bst.resize(n + 1, false);
+        inDegree.resize(n + 1, 0);
+        top.resize(n + 1, 0);
+        bstHere.resize(n + 1, false);
 
-        //初始化顶点
         for (int i = 0; i <= n; i++)
             nodes.emplace_back(Node());
-        //读入边
         for (int i = 1; i <= m; i++) {
             int u, v, w;
             cin >> u >> v >> w;
             nodes[u].edges.emplace_back(v, w);
             //修改入度
-            in_deg[v]++;
-            nodes[u].max_to_cost = max(nodes[u].max_to_cost, w);
+            inDegree[v]++;
+            nodes[u].maxToCost = max(nodes[u].maxToCost, w);
         }
         nodes[1].press = Pmax;
-        place_bst[1] = true;
+        bstHere[1] = true;
     }
     ~DAG() {}
-    int best_ans;//最优解 数目
+    int maxAns;//最优解 数目
 
     void topsort();//用拓扑排序 来确定先后关系
     void output_bst();//输出booster
-    void backtracking(int level, int cnt);//回溯解决
-    void branch_bound();//分支定界解决
+    void backTrace(int level, int cnt);//回溯解决
+    void branchBound();//分支定界解决
 
 };
 
 
 void DAG::topsort()
-{//确定一个拓扑序列 以便后续
+{
     int cnt = 1;
     queue<int> q;
     for (int i = 1; i <= n; i++) {
-        if (in_deg[i] == 0) q.push(i);
+        if (inDegree[i] == 0) q.push(i);
     }
     while (!q.empty()) {
         int u = q.front(); q.pop();
-        dig[cnt++] = u;//排序
+        top[cnt++] = u;//排序
         for (auto e : nodes[u].edges) {
-            if (--in_deg[e.to] == 0)
+            if (--inDegree[e.to] == 0)
                 q.push(e.to);
         }
     }
-    if (cnt < n) {
-        cout << "图中有环路！不符合要求！\n";
-        return;
-    }
 }
 
 
-// 回溯需要按照拓扑序跑！ dfs
-void DAG::backtracking(int level, int cnt)
-{//进行回溯决策树里面 左是该节点不放 右子树是放
-    //搜索第level层
-    int u = dig[level];//当前点 level也代表了拓扑序
-
-    if (level >= n) {//最底层了
-        best_ans = min(cnt, best_ans);
-        //记录下最好的情况 不然会被冲了
-        //更新放置情况
-        for (int i = 1; i <= n; i++) {
-            place_bst[i] = false;
-            place_bst[i] = nodes[i].booster;
-        }
-        return;
-    }
-
-    if (level == 1) {//第一层 源点肯定放了（不算）只需要判断其他边 如果低了 就在v放
-        backtracking(2, 0);
-    }
-    else {//其他层  如果到v 不行 则在 u放
-        int maxPress = -1;//临时变量pre 用来回溯！
-        for (int k = 1; k < level; k++) {//求 前面到
-            for (auto& x : nodes[dig[k]].edges) {
-                if (x.to == u)
-                    maxPress = max(maxPress, nodes[dig[k]].press - x.cost);
-            }
-        }
-
-        if (maxPress >= Pmax) {
-            backtracking(level + 1, cnt);
-            return;
-        }
-        if(maxPress - nodes[u].max_to_cost >= Pmin)
-        {
-            nodes[u].press = maxPress;
-            nodes[u].booster = false;
-            backtracking(level + 1, cnt);
-        }
-        cnt++;
-        nodes[u].press = Pmax;
-        nodes[u].booster = true;
-        //剪枝
-        if (cnt >= best_ans)
-            return;
-        else {
-            backtracking(level + 1, cnt);
-        }
-    }
-}
-
-//最小耗费 分支定界
-void DAG::branch_bound()
+void DAG::backTrace(int level, int cnt)
 {
-    //分支定界 活结点--扩展结点-- 子集空间树
-    btnode* enode = new btnode(Pmax, 2);//活结点
+    if (level >= n) {
+        maxAns = min(cnt, maxAns);
+        for (int i = 1; i <= n; i++) {
+            bstHere[i] = false;
+            bstHere[i] = nodes[i].bstHere;
+        }
+        return;
+    }
+
+    int u = top[level];
+    int maxPress = -1;
+    for (int k = 1; k < level; k++) {
+        for (auto& x : nodes[top[k]].edges) {
+            if (x.to == u)
+                maxPress = max(maxPress, nodes[top[k]].press - x.cost);
+        }
+    }
+
+    if (maxPress >= Pmax) {
+        backTrace(level + 1, cnt);
+        return;
+    }
+    if(maxPress - nodes[u].maxToCost >= Pmin)
+    {
+        nodes[u].press = maxPress;
+        nodes[u].bstHere = false;
+        backTrace(level + 1, cnt);
+    }
+    cnt++;
+    nodes[u].press = Pmax;
+    nodes[u].bstHere = true;
+    if (cnt < maxAns)
+        backTrace(level + 1, cnt);
+}
+
+
+void DAG::branchBound()
+{
+    btnode* node = new btnode(Pmax, 2);
     int level = 2;
     while (level < n)
-    {//进行活结点的拓展
-        int vert = dig[level];//该层扩展的节点
+    {
+        int vert = top[level];
         int maxPress = -1;
 
-        //dig[k]：拓扑序为k的点
-        //求扩展结点的压力
         for (int k = 1; k < level ; k++) {
-            for (auto& e : nodes[dig[k]].edges) {
+            for (auto& e : nodes[top[k]].edges) {
                 if (e.to == vert) {
-                    btnode* p = enode;
-                    //????
+                    btnode* p = node;
                     for (int j = level - 1; j > k; j--)
                         p = p->parent;
 
@@ -200,31 +176,30 @@ void DAG::branch_bound()
             }
         }
 
-        if (maxPress - nodes[vert].max_to_cost >= Pmin) {
-            btnode* t = new btnode(maxPress, level + 1, enode, enode->bstnum);
+        if (maxPress - nodes[vert].maxToCost >= Pmin) {
+            btnode* t = new btnode(maxPress, level + 1, node, node->bstCnt);
             pq.push(t);
         }
-        btnode* tt = new btnode(Pmax, level + 1, enode, enode->bstnum + 1);
-        tt->bst_here = true;
+        btnode* tt = new btnode(Pmax, level + 1, node, node->bstCnt + 1);
+        tt->bstHere = true;
         pq.push(tt);
 
-        enode = pq.top(); pq.pop();
-        level = enode->level;
+        node = pq.top(); pq.pop();
+        level = node->level;
     }
-    best_ans = enode->bstnum;
+    maxAns = node->bstCnt;
 
-    btnode* p = enode;
+    btnode* p = node;
     while (p) {
-        place_bst[dig[p->level - 1]] = p->bst_here;
+        bstHere[top[p->level - 1]] = p->bstHere;
         p = p->parent;
     }
-
 }
 
 void DAG::output_bst()
 {
     for (int i = 1; i <= n; i++) {
-        if (place_bst[i])
+        if (bstHere[i])
             cout << "node " << i << " ";
     }
 }
